500519760b4ad29f74d4372b59014d83e170c04c
[SW4/N반] 김윤중 <qpdlql7860@gmail.com>
2023-10-02 01:44:45 +0900

    'test'

---
+++ src/pages/Diary.js
@@ -0,0 +1,13 @@
+import Calendar from '../components/Calendar/Calendar';
+import Header from '../components/Header/Header';
+
+function Diary() {
+  return (
+    <div>
+      <Header />
+      <Calendar />
+    </div>
+  );
+}
+
+export default Diary;
